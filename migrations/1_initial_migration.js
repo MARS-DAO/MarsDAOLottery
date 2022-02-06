4c7aea557810917114f37321ce11c054a271a88a
DAOmars <marsdao@protonmail.com>
2022-02-06 14:55:13 +0000

    fix deploy to testnet

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -19,12 +19,14 @@ module.exports = async (deployer, network) => {
     const VRFCoordinator="0xa555fC018435bef5A13C6c6870a9d4C11DEC329C";
     const LINKToken="0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06";
     const feeAdr="0x53D8481C4ee35bC789E1acaB7129e8dF1205C519";
+    const keyHash="0xcaf3c3727e033261d383b315559476f48034c13b18f8cafed4d871abe5049186";
     try{
       const fee=web3.utils.toWei("0.1", "ether");
       await deployer.deploy(MarsTpokenERC20,'newMars', 'newMars', web3.utils.toWei("10000000", "ether"));
       await deployer.deploy(MarsDAOLottery,MarsTpokenERC20.address,feeAdr,VRFCoordinator,LINKToken);
       const MarsDAOLotteryInstance = await MarsDAOLottery.deployed();
       await MarsDAOLotteryInstance.setRandomOracleFee(fee);
+      await MarsDAOLotteryInstance.setKeyHash(keyHash);
     }catch(err){
       console.log("ERROR:",err);
     }
